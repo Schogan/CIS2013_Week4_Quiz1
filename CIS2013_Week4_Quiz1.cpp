6540077a14c470a8a74335a979e37b45bf8f0c3d
Schogan <lrschanbacher@gmail.com>
2018-02-07 18:53:21 -0600

    Counts the FOO and BAR

--- CIS2013_Week4_Quiz1.cpp
+++ CIS2013_Week4_Quiz1.cpp
@@ -4,17 +4,21 @@ using namespace std;
 
 int main(){
 	int i = 0;
+	int FOO = 0;
+	int BAR = 0;
 	for (i=0;i<=2000;i++){
 		cout << i;
 		
 		if (i % 50 == 0){
 			cout << " FOO";
+			FOO = FOO + 1;
 		}
 		if (i % 33 == 0){
-			cout << " BAR";
+			cout << " BAR"; 
+			BAR = BAR + 1;
 		}
-		
 		cout << endl;
 	}
-	
+	cout << "Your FOO total is: " << FOO << endl;
+	cout << "Your BAR total is: " << BAR << endl;
 }
